216ca1a4b653824de6fcc44e40dbb02cc86cd48a
tvariterp <tvariterp@gmail.com>
2024-12-06 12:38:04 +0530

    3

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello World! 😁 🎈');
+});
+
 const routs = require('./routes/routes_index');
 app.use('/api', routs);
 
